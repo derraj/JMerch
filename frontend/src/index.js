268515399539744e8ac326fde9e4a104b54a08da
jarredcm <mahinay@ualberta.ca>
2022-05-12 15:41:45 -0600

    Added redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import Product from '../components/Product.js'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { error, loading, products } = productList;
 
   useEffect(() => {
-
-    async function fetchProducts() {
-      const { data } = await axios.get('/api/products/');
-      setProducts(data);
-    }
-
-    fetchProducts();
-    
-  },[])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+      {loading ? <Loader/>
+        : error ? <Message/>
+          :
+          <Row>
+            {products.map(product => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+      }
+
     </div>
   )
 }
